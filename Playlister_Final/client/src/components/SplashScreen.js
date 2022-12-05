8fbc810990758959ee48780aaf816929b00a89de
GreenMarioX <91627412+GreenMarioX@users.noreply.github.com>
2022-12-05 00:17:43 -0500

    Addition: Splash Screen

--- Playlister_Final/client/src/components/SplashScreen.js
+++ Playlister_Final/client/src/components/SplashScreen.js
@@ -1,7 +1,16 @@
+import { Typography, Box } from '@mui/material';
+
 export default function SplashScreen() {
     return (
-        <div id="splash-screen">
-            Playlister
-        </div>
-    )
+        <Typography id="splash-screen">
+            <Typography style={{ color:'red', fontFamily: 'Tangerine', fontSize: 100 }}>
+                Playlister
+            </Typography>
+            <Typography paragraph={true} align="center">
+                Create playlists and share your favorite songs
+                with others
+            </Typography>
+        </Typography>
+
+    )  
 }
