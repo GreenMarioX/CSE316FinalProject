10cb73b087ebae1c711c4359f02ff5ac42439113
GreenMarioX <91627412+GreenMarioX@users.noreply.github.com>
2022-12-05 05:18:38 -0500

    Modification: Comments

--- Playlister_Final/client/src/components/CommentListCard.js
+++ Playlister_Final/client/src/components/CommentListCard.js
@@ -1,39 +1,42 @@
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import { GlobalStoreContext } from '../store/index.js'
 import { List, TextField, Box } from '@mui/material';
-import SongCard from './SongCard.js';
+import CommentCard from './CommentCard.js';
+import AuthContext from '../auth';
 
 function CommentListCard() {
     const { store } = useContext(GlobalStoreContext);
-    const [x, setX] = useState(1);
-    let page;
+    const { auth } = useContext(AuthContext);
+    let display;
 
-    function handleEnter (event) {
-        if(event.key === 'Enter') {
-            setX(2);
+    function handleEnter(event) {
+        if(event.key === "Enter") {
+            let comment = event.target.value;
+            let userName = auth.user.firstName + " " + auth.user.lastName;
+            store.addCommentLikeDislikeListen(userName, comment, false, false, false, store.currentList._id);
         }
     }
 
     if(store.currentList) {
-        page = 
-            <Box sx={{ height: 350}}>
-                <List id="playlist-cards" sx={{ pd: 5, left: '2.5%', height: '100%', width: '95%', bgcolor: '#eeeeedd', overflowY: 'auto' }}>
-                    {x}
+        display = 
+            <Box sx={{ height: 350}} >
+                <List id="playlist-cards" sx={{ pd: 5, mr: 1, height: '100%', width: '100%', bgcolor: '#eeeeedd', overflowY: 'auto', overflowX: 'hidden' }}>
+                    {store.currentList.comments.map((comment) => (<CommentCard id='playlist-comment' comment={comment}/>))}
                 </List>
-                <TextField id="outlined-basic" label="Add Comment" variant="outlined" sx={{ width: '100%', backgroundColor: 'white', mt: 1}}onKeyPress={handleEnter}/>
+                <TextField id="outlined-basic" label="Add Comment" sx={{ width: '100%', backgroundColor: 'white', mt: 1}} onKeyPress={handleEnter}/> 
             </Box>
     } 
     else {
-        page =
+        display =
         <Box sx={{ height: 350}}>
         <List id="playlist-cards" sx={{ pd: 5, left: '2.5%', height: '100%', width: '95%', bgcolor: '#eeeeedd', overflowY: 'auto' }}>
         </List>
-        <TextField id="outlined-basic" label="Add Comment" variant="outlined" sx={{ width: '100%', backgroundColor: 'white', mt: 1}} onKeyPress={handleEnter}/> 
+        <TextField id="outlined-basic" label="Add Comment" variant="outlined" sx={{ width: '100%', backgroundColor: 'white', mt: 1}}/> 
         </Box>
     }
 
     return (
-        page
+        display
     )
 }
 
