781ec4fedbd3445fabcc7a8df4327d1149bf51f9
GreenMarioX <91627412+GreenMarioX@users.noreply.github.com>
2022-12-05 07:50:06 -0500

    Readability: Cleaned up Code

--- Playlister_Final/client/src/App.js
+++ Playlister_Final/client/src/App.js
@@ -8,19 +8,9 @@ import {
     HomeWrapper,
     LoginScreen,
     RegisterScreen,
-    Statusbar
+    Statusbar,
 } from './components'
-/*
-    This is our application's top-level component.
-    
-    @author McKilla Gorilla
-*/
-/*
-  This is the entry-point for our application. Notice that we
-  inject our store into all the components in our application.
-  
-  @author McKilla Gorilla
-*/
+
 const App = () => {   
     return (
         <BrowserRouter>
@@ -31,7 +21,6 @@ const App = () => {
                         <Route path="/" exact component={HomeWrapper} />
                         <Route path="/login/" exact component={LoginScreen} />
                         <Route path="/register/" exact component={RegisterScreen} />
-
                     </Switch>
                     <Statusbar />
                 </GlobalStoreContextProvider>
