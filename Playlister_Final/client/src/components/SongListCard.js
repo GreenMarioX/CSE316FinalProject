c72014d2859c59797ca42ab72edcd322e011027a
GreenMarioX <91627412+GreenMarioX@users.noreply.github.com>
2022-12-05 07:24:03 -0500

    Modification: Song Cards

--- Playlister_Final/client/src/components/SongListCard.js
+++ Playlister_Final/client/src/components/SongListCard.js
@@ -1,46 +1,33 @@
 import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
 import SongCard from './SongCard.js'
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
+import { Box, List} from '@mui/material'
 import { GlobalStoreContext } from '../store/index.js'
 import MUIEditSongModal from './MUIEditSongModal'
 import MUIRemoveSongModal from './MUIRemoveSongModal'
-/*
-    This React component lets us edit a loaded list, which only
-    happens when we are on the proper route.
-    
-    @author McKilla Gorilla
-*/
-function SongListCard() {
+
+
+function SongListCard(props) {
     const { store } = useContext(GlobalStoreContext);
+    const { songs } = props;
     store.history = useHistory();
     let display;
 
     let modalJSX = "";
     if (store.isEditSongModalOpen()) {
-        modalJSX = <MUIEditSongModal />;
+        modalJSX = <MUIEditSongModal/>;
     }
     else if (store.isRemoveSongModalOpen()) {
-        modalJSX = <MUIRemoveSongModal />;
+        modalJSX = <MUIRemoveSongModal/>;
     }
 
-    if(store.currentList) {
-        display = 
+    display = 
         <Box>
             <List id="playlist-cards" sx={{ pd: 5, left: '2.5%', height: '100%', width: '95%', bgcolor: '#eeeeedd'  }}>
-                {
-                    store.currentList.songs.map((song, index) => (
-                        <SongCard id={'playlist-song-' + (index)} key={'playlist-song-' + (index)} index={index} song={song}/>
-                    ))  
-                }
+                {songs.map((song, index) => (<SongCard id={'playlist-song-' + (index)} key={'playlist-song-' + (index)} index={index} song={song}/>))}
             </List>
             {modalJSX}
          </Box>            
-    } 
-    else {
-        display = <Box> </Box>
-    }
 
     return (
         display
